ddb1a4af1a0b2070be4c31f19737b39863f44632
Iswarya-code <ishujeni6@gmail.com>
2025-04-03 13:43:16 +0530

    added mainMenu with nickname

--- Network_3DShooter/Assets/Scripts/GameManager.cs
+++ Network_3DShooter/Assets/Scripts/GameManager.cs
@@ -2,19 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public InputField playerNickname;
+    string setName = "";
+    public GameObject connecting;
+
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.AutomaticallySyncScene = true;
-        PhotonNetwork.ConnectUsingSettings();
+        connecting.SetActive(false);
+      
     }
 
     // Update is called once per frame
-    void Update()
+   public void UpdateText()
     {
-        
+        setName = playerNickname.text;
+        PhotonNetwork.LocalPlayer.NickName = setName;
+    }
+
+    public void EnterButton()
+    {
+        if(setName != "")
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings();
+            connecting.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 
     public override void OnConnectedToMaster()
--- Network_3DShooter/Assets/Scripts/LookAt.cs
+++ Network_3DShooter/Assets/Scripts/LookAt.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class LookAt : MonoBehaviour
 {
     Vector3 worldPosition;
     Vector3 screenPosition;
     public GameObject crossHair;
+    public Text nickNameText;
 
     private void Start()
     {
         Cursor.visible = false;
+        nickNameText.text = PhotonNetwork.LocalPlayer.NickName;
     }
     // Update is called once per frame
     void FixedUpdate()
